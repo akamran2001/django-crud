3c6c9eb3df88e34a86a630cec901efee3314e034
Ahmed Kamran <akamran@colgate.edu>
2022-03-19 19:44:13 -0400

    server cleaned up

--- serve.py
+++ serve.py
@@ -4,9 +4,7 @@
 # Make sure to edit the DJANGO_SETTINGS_MODULE to point to your settings.py
 #
 
-
 import os
-from tornado.options import options, define, parse_command_line
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
@@ -16,26 +14,17 @@ import CRUD.asgi
 PORT = 8080
 
 
-class HelloHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('Hello from tornado')
-
-
 def main():
     os.environ['DJANGO_SETTINGS_MODULE'] = 'CRUD.settings'
-    parse_command_line()
     asgi_app = CRUD.asgi.application
-
     tornado_app = tornado.web.Application(
         [
             ("/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static'}),  # Serve Static Files
             ("/media/(.*)", tornado.web.StaticFileHandler, {'path': 'media'}),  # Serve Media Files
             ('.*', tornado_asgi_handler.AsgiHandler, dict(asgi_app=asgi_app)),  # Serve Django Application
         ])
-
     server = tornado.httpserver.HTTPServer(tornado_app)
     server.listen(PORT)
-
     tornado.ioloop.IOLoop.instance().start()
 
 
